91c70b6661f414868a177fc6d15c9a2b7b32d67e
shk1999 <shayan000karimi@gmail.com>
2025-06-30 17:44:21 +0330

    update(velero-install): use correct version

--- bin/install-velero.sh
+++ bin/install-velero.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Install Velero and configure MinIO backend
 
-MINIO_ENDPOINT=$1
-MINIO_ACCESS_KEY=$2
-MINIO_SECRET_KEY=$3
-MINIO_BUCKET=$4
+MINIO_ENDPOINT="http://<your-minio-endpoint>:<port>"
+MINIO_ACCESS_KEY=<your-access-key>
+MINIO_SECRET_KEY=<your-secret-key>
+MINIO_BUCKET=<velero-backup>
 
 # Download and install Velero
-wget https://github.com/vmware-tanzu/velero/releases/download/v1.9.0/velero-v1.9.0-linux-amd64.tar.gz
-tar -xvf velero-v1.9.0-linux-amd64.tar.gz
-sudo mv velero-v1.9.0-linux-amd64/velero /usr/local/bin/
+wget https://github.com/vmware-tanzu/velero/releases/download/v1.16.1/velero-v1.16.1-linux-amd64.tar.gz
+tar -xvf velero-v1.16.1-linux-amd64.tar.gz
+sudo mv velero-v1.16.1-linux-amd64/velero /usr/local/bin/
 
 # Create credentials file for MinIO
 cat <<EOF > minio-credentials
@@ -21,7 +21,7 @@ EOF
 # Install Velero with MinIO backend
 velero install \
     --provider aws \
-    --plugins velero/velero-plugin-for-aws:v1.5.0 \
+    --plugins velero/velero-plugin-for-aws:v1.12.1 \
     --bucket $MINIO_BUCKET \
     --secret-file ./minio-credentials \
     --backup-location-config region=minio,s3ForcePathStyle="true",s3Url=$MINIO_ENDPOINT \
